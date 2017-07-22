74f62f2d8e7523d15b03e034995a7b132e3c3da5
ueyuki <s14t212@stmail.eng.kagawa-u.ac.jp>
2017-07-23 02:52:52 +0900

    Refactaring no546.rb

--- star3/no546.rb
+++ star3/no546.rb
@@ -1,38 +1,24 @@
-def gcd(a, b)
-  if b > 0
-    gcd(b, a % b)
-  else
-    return a
-  end
-end
-
-def lcm(a, b)
-  a * b / gcd(a, b)
-end
-
-
 n, l, h = gets.chomp.split.map(&:to_i)
-c = gets.chomp.split.map(&:to_i)
+cc = gets.chomp.split.map(&:to_i) 
 
-aunswer = 0
+l -= 1
+num = 0
 
-0.upto(n-1) do |i|
-  aunswer += ((h / c[i]) - (l - 1) / c[i])
+cc.each do |i|
+  num += (h / i) - (l / i)
 end
 
 2.upto(n) do |i|
-  c.combination(i) { |com|
-    lc = com.inject(1) { |a, b|
-      lcm(a, b)
+  cc.combination(i) { |combi|
+
+    lcm = combi.inject { |a, b|
+      a.lcm(b)
     }
 
-    tmp = (h / lc) - ((l - 1) / lc)
-    if i % 2 == 0
-      aunswer += tmp * (i * -1)
-    else
-      aunswer += tmp * i
-    end
+  commonNum = (h / lcm) - (l / lcm)
+  num += commonNum * (i.odd?? i : -i)
+
   }
 end
 
-puts aunswer
+puts num
