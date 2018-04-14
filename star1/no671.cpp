aa5b561ea9596b121d1b1108d08b99fdd53dffd4
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-14 21:31:31 +0900

    AC No.671

---
+++ star1/no671.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  string N;
+  int diff;
+
+  cin >> N;
+
+  diff = 10 - n.size();
+
+  printf("%d\n", abs(diff));
+
+}
